dbde5afc1463d880c7d7e2fa9c7723204d854e48
Jack Danger <github@jackdanger.com>
2026-03-09 06:07:37 -0700

    Expand MediaPackageV2: add exception handling and response routes

--- moto/mediapackagev2/exceptions.py
+++ moto/mediapackagev2/exceptions.py
@@ -19,3 +19,27 @@ class ChannelGroupNotFound(JsonRESTError):
     def __init__(self) -> None:
         msg = "MediaPackage can't process your request because we can't find your channel group. Verify your channel group name or add a channel group and then try again"
         super().__init__(error_type="ResourceNotFoundException", message=msg)
+
+
+class OriginEndpointNotFound(JsonRESTError):
+    def __init__(self) -> None:
+        msg = "MediaPackage can't process your request because we can't find your origin endpoint. Verify your origin endpoint name or add an origin endpoint and then try again"
+        super().__init__(error_type="ResourceNotFoundException", message=msg)
+
+
+class HarvestJobNotFound(JsonRESTError):
+    def __init__(self) -> None:
+        msg = "MediaPackage can't process your request because we can't find your harvest job. Verify your harvest job name and try again"
+        super().__init__(error_type="ResourceNotFoundException", message=msg)
+
+
+class OriginEndpointPolicyNotFound(JsonRESTError):
+    def __init__(self) -> None:
+        msg = "MediaPackage can't process your request because we can't find the policy for this origin endpoint"
+        super().__init__(error_type="ResourceNotFoundException", message=msg)
+
+
+class ChannelPolicyNotFound(JsonRESTError):
+    def __init__(self) -> None:
+        msg = "MediaPackage can't process your request because we can't find the policy for this channel"
+        super().__init__(error_type="ResourceNotFoundException", message=msg)
