487f1a97a8a62facfca766ee93b14dd92f117462
Jack Danger <github@jackdanger.com>
2026-03-20 01:59:51 -0700

    feat(account): add URL routes for new response handlers

--- moto/account/urls.py
+++ moto/account/urls.py
@@ -10,4 +10,13 @@ url_paths = {
     "{0}/putAlternateContact$": AccountResponse.dispatch,
     "{0}/getAlternateContact$": AccountResponse.dispatch,
     "{0}/deleteAlternateContact$": AccountResponse.dispatch,
+    "{0}/getAccountInformation$": AccountResponse.dispatch,
+    "{0}/getContactInformation$": AccountResponse.dispatch,
+    "{0}/putContactInformation$": AccountResponse.dispatch,
+    "{0}/getGovCloudAccountInformation$": AccountResponse.dispatch,
+    "{0}/getPrimaryEmail$": AccountResponse.dispatch,
+    "{0}/getRegionOptStatus$": AccountResponse.dispatch,
+    "{0}/enableRegion$": AccountResponse.dispatch,
+    "{0}/disableRegion$": AccountResponse.dispatch,
+    "{0}/listRegions$": AccountResponse.dispatch,
 }
