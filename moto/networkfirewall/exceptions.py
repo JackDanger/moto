51450b80f69c3d8d74d77115b0f5f2f661e6d21f
Jack Danger <github@jackdanger.com>
2026-03-25 19:47:59 -0700

    fix: include __type in networkfirewall exceptions for proper error code parsing

--- moto/networkfirewall/exceptions.py
+++ moto/networkfirewall/exceptions.py
@@ -6,28 +6,40 @@ from moto.core.exceptions import JsonRESTError
 
 
 class ResourceNotFound(JsonRESTError):
+    code = 400
+
     def __init__(self, resource_id: str):
         super().__init__("ResourceNotFoundException", "Resource not found.")
-        body = {
-            "ResourceId": resource_id,
-            "Message": "Resource not found.",
-        }
-        self.description = json.dumps(body)
+        self.description = json.dumps(
+            {
+                "__type": "ResourceNotFoundException",
+                "Message": "Resource not found.",
+                "ResourceId": resource_id,
+            }
+        )
 
 
 class ResourceInUse(JsonRESTError):
+    code = 400
+
     def __init__(self, message: str):
         super().__init__("ResourceOwnerCheckException", message)
-        body = {
-            "Message": message,
-        }
-        self.description = json.dumps(body)
+        self.description = json.dumps(
+            {
+                "__type": "ResourceOwnerCheckException",
+                "Message": message,
+            }
+        )
 
 
 class InvalidRequest(JsonRESTError):
+    code = 400
+
     def __init__(self, message: str):
         super().__init__("InvalidRequestException", message)
-        body = {
-            "Message": message,
-        }
-        self.description = json.dumps(body)
+        self.description = json.dumps(
+            {
+                "__type": "InvalidRequestException",
+                "Message": message,
+            }
+        )
